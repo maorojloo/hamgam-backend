ca0745de3bea2d50e2dd4b9dc790d889b678c523
khoramism <khoramism@gmail.com>
2022-06-22 10:10:10 +0430

    Redis for caching, validators for passwords, debug_toolbar, dotenv, loggers

--- hamgam/hamgam-backend/hamgam/settings.py
+++ hamgam/hamgam-backend/hamgam/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import socket
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR + '/.env'
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External 
+    "debug_toolbar",
+    'rest_framework',
+    
+    # Internal 
+    'account.apps.AccountConfig',
+    'idea.apps.IdeaConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +60,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Externals
+    # Debug toolbar  
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # LEAKED PASSWORDS 
+    'pwned_passwords_django.middleware.PwnedPasswordsMiddleware',
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'hamgam.urls'
@@ -69,7 +90,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hamgam.wsgi.application'
 
-
+if DEBUG:
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -96,7 +119,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    }, 
+    {
+        'NAME': 'account.validators.ContextValidator'
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+    },
+    {
+        'NAME':'pwned_passwords_django.validators.PwnedPasswordsValidator',
+        'OPTIONS': {'error_message': ('این رمز عبور قبلا هک شده بوده. برای اطلاعات بیشتر /n https://haveibeenpwned.com/ /n  یک سری بزنید')}
+    }
 ]
 
 
@@ -117,9 +150,96 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# URL 
+ALLOW_UNICODE_SLUGS = True
+
+#### EMIAL STUFF
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Account Models
+AUTH_USER_MODEL = 'account.Account'
+
+
+# Logging 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': f'{BASE_DIR}/../../logs/debug.log',
+        }
+
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+## Coolkie Sessions 
+
+
+# Rest FrameWork 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework.authentication.SessionAuthentication', 
+
+    ), 
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissons.IsAuthenticatedOrReadOnly',
+    ),
+}
+
+
+# Email 
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_email')
+
+
+
+# Media 
+
+MEDIA_URL = "/media/"
+# any file field upload by default
+MEDIA_ROOT =  f"{BASE_DIR}/cdn_test/media"
+
+PROTECTED_MEDIA =  f"{BASE_DIR}/cdn_test/protected"
+
+
+# Caching Using Redis 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@127.0.0.1:6379',
+    },
+    'OPTIONS': {
+    'db': '0',
+    'parser_class': 'redis.connection.PythonParser',
+    'pool_class': 'redis.BlockingConnectionPool', 
+    }
+}
+
+#### NOT ADDING NOW 
+## adding uWSGI
+# sudo apt-get install uwsgi uwsgi-plugin-python uwsgi-plugin-cgi
