5d2a3813f614f1422709de9d94f0fcfae83df291
ma.orojloo <ma.orojloo@gmail.com>
2022-06-27 21:20:13 +0430

    adding some stuff to dev.txt & creating appeal app model

--- hamgam/hamgam-backend/appeal/models.py
+++ hamgam/hamgam-backend/appeal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from config import shared
 # Create your models here.
+
+
+class Petition(shared.TimeStampedModel):
+
+    account_id = models.ForeignKey(
+        "account.Account", on_delete=models.CASCADE, related_name='Petitions')
+    idea_id = models.ForeignKey(
+        "idea.Idea", on_delete=models.CASCADE, related_name='Petitions')
+    address = models.ForeignKey('app_label.Address', on_delete=models.CASCADE)
+    content = models.TextField(max_length=255)
+    resume_link = models.URLField()
+    active = models.BooleanField(default=True)
+
+    objects = shared.ActiveManager()
